c06a35d8973da871a1ef2ec3941e652031a8138e
monorainbow <monoholic00@gmail.com>
2024-09-10 16:28:44 +0900

    # DrawerBar 구성완료

--- src/layouts/DrawerLayout.vue
+++ src/layouts/DrawerLayout.vue
@@ -2,117 +2,109 @@
   <q-scroll-area class="fit">
     <q-list>
       <q-item-label header> Menu Bar </q-item-label>
-      <div v-for="menu in menuList" :key="menu.menuId">
-        <q-expansion-item expand-separator icon="mail" :label="menu.menuNm">
+      <div v-for="menu in state.menuList" :key="menu.menuId">
+        <q-expansion-item expand-separator :icon="menu.menuIcon" :label="menu.menuNm">
 
-          <div v-for="subMenu in subMenuList" :key="subMenu.menuId">
+          <div v-for="subMenu in state.subMenuList" :key="subMenu.menuId">
+
             <q-expansion-item
               :header-inset-level="1"
-              icon="receipt"
+              :icon="subMenu.menuIcon"
               :label="subMenu.menuNm"
-              v-if="menu.menuId === subMenu.uppMenuId"
+              v-if="menu.menuId === subMenu.uppMenuId && subMenu.routeYn === 'Y'"
             >
-              <q-item clickable :label="subMenu.menuNm">
-                  <q-item-section avatar>
-                    <q-icon :name="subMenu.menuIcon" />
-                  </q-item-section>
-                <q-item-section> {{subMenu.menuDesc}} </q-item-section>
-              </q-item>
+              <div v-for="thirdMenu in state.thirdMenuList" :key="thirdMenu.menuId">
+                <q-item 
+                  :inset-level="2" 
+                  :label="thirdMenu.menuNm" 
+                  v-if="subMenu.menuId === thirdMenu.uppMenuId" 
+                  v-on:click="pageMove(thirdMenu.url)"
+                  clickable 
+                >
+                    <q-item-section avatar>
+                      <q-icon :name="thirdMenu.menuIcon" />
+                    </q-item-section>
+                  <q-item-section> {{thirdMenu.menuNm}} </q-item-section>
+                </q-item>
+              </div>
+
             </q-expansion-item>
+
+            <q-item
+              :inset-level="1"
+              :label="subMenu.menuNm" 
+              v-else-if="menu.menuId === subMenu.uppMenuId && subMenu.routeYn === 'N'" 
+              v-on:click="pageMove(subMenu.url)"
+              clickable 
+            >
+                <q-item-section avatar>
+                  <q-icon :name="subMenu.menuIcon" />
+                </q-item-section>
+              <q-item-section> {{subMenu.menuNm}} </q-item-section>
+            </q-item>
 
           </div>
         </q-expansion-item>
+
       </div>
     </q-list>
   </q-scroll-area>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { onMounted, reactive } from 'vue';
+import { api } from "boot/axios";
+import { useRouter } from 'vue-router'
 
-export default defineComponent({
-  setup() {
-    return {};
-  },
-  components: {},
-  data() {
-    return {
+  export default {
+    setup (){
 
-      menuList: [],
-      subMenuList: [],
+      const router = useRouter()
 
-      isLoading: false,
-      fullPage: true,
-    };
-  },
+      onMounted(async() => {
+        getMenuList()
+      })
 
-  created () {
-    this.getMenuList();
-  },
+      const state = reactive({
+        menuList: [],
+        subMenuList: [],
+        thirdMenuList: [],
+      })
+    
+      const getMenuList = () => {
 
-  methods: {
-    pageMove(route) {
-      this.$router.push(route);
-    },
-    getMenuList() {
-
-      this.$api
+        api
         .get("/menu/menuList")
         .then((res) => {
           if (res.data.code === 200) {
-
-            console.log('====> res : ', JSON.stringify(res.data.data));
-
-            this.menuAppend(res.data.data);
-            // this.menuList = res.data.data;
-
-          } else {
-            console.log("err =====> ", res.data.code)
-          }
-          this.isLoading = false;
+            menuAppend(res.data.data);
+          } 
         })
         .catch((error) => {
-          console.log(" error ==>", error);
-          this.isLoading = false;
         })
         .finally(() => {
           console.log("항상 마지막에 실행");
         });
 
-        console.log('======> ',localStorage.getItem('token'));
+      }
 
-        localStorage.removeItem('token');
+      const pageMove = (pagePath) => {
+        router.push({name : pagePath});
+      }
 
-    },
+      const menuAppend = (data) => {
+        state.menuList = data.filter(menu => menu.menuLvl === '1');
+        state.subMenuList = data.filter(menu => menu.menuLvl === '2');
+        state.thirdMenuList = data.filter(menu => menu.menuLvl === '3');
+      }
 
-    menuAppend(menuList){
-
-      this.menuList = menuList.filter(menu => menu.menuLvl === '1');
-      this.subMenuList = menuList.filter(menu => menu.menuLvl !== '1');
-
-      // let subMenuList = [];
-
-      // for(var i =0; i<menuList.length; i++){
-
-      //   if(menuList[i].menuLvl === '1'){
-      //     console.log('menuList ===> ', JSON.stringify(menuList[i]));
-      //     this.menuList.push(menuList[i]);
-      //   }
-
-      //   for(var j = 0;j<menuList.length; j++){
-      //     if(menuList[j].menuLvl === '2' && menuList[j].uppMenuId === menuList[i].menuId){
-      //       console.log('=> ', JSON.stringify(menuList[j]));
-      //       subMenuList.push(menuList[j]);
-      //       console.log(' subMenuList ==> ', JSON.stringify(subMenuList));
-      //     }
-
-      //     this.menuList.fill(subMenuList);
-      //   }
-      // }
-
-      console.log('final =====> ', JSON.stringify(this.menuList));
-
+      return {
+        getMenuList,
+        pageMove,
+        menuAppend,
+        state,
+      }
     }
-  },
-});
+  }
+
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,57 @@
 const routes = [
   {
+    name: "",
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   {
+    name: "list",
     path: "/list",
     component: () => import("layouts/ListLayout.vue"),
   },
   {
-    path: "/login",
-    component: () => import("layouts/LoginLayout.vue"),
+    name: "/approval",
+    path: "/approval",
+    component: () => import("layouts/ApprovalLayout.vue"),
+  },
+  {
+    name: "/receiveApproval",
+    path: "/receiveApproval",
+    component: () => import("layouts/ReceiveApprLayout.vue"),
+  },
+  {
+    name: "/commonCode",
+    path: "/commoncode",
+    component: () => import("layouts/CommonCodeLayout.vue"),
+  },
+  {
+    name: "/roleManage",
+    path: "/roleManage",
+    component: () => import("layouts/RoleManageLayout.vue"),
+  },
+  {
+    name: "/menuManage",
+    path: "/menuManage",
+    component: () => import("layouts/MenuManageLayout.vue"),
+  },
+  {
+    name: "/logManage",
+    path: "/logManage",
+    component: () => import("layouts/LogManageLayout.vue"),
+  },
+  {
+    name: "/userChange",
+    path: "/userChange",
+    component: () => import("layouts/UserChangeLayout.vue"),
   },
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
+  },
+  {
+    path: "/login",
+    component: () => import("layouts/LoginLayout.vue"),
   },
 ];
 
